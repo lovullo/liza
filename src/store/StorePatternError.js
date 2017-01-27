b62673791b176f2f06b902f1c576b78cea3fdfa5
Mike Gerwitz <gerwitzm@lovullo.com>
2017-01-27 11:07:01 -0500

    Add PatternProxy Store trait
    Life is so much less miserable now that the project is supporting ES6.
    
    * src/store/PatternProxy.js: Add trait.
    * src/store/StorePatternError.js: Add Error.
    * test/store/PatternProxyTest.js: Add test case.
    
    DEV-2296

---
+++ src/store/StorePatternError.js
@@ -0,0 +1,31 @@
+/**
+ * Error when Store pattern matching fails
+ *
+ *  Copyright (C) 2017 LoVullo Associates, Inc.
+ *
+ *  This file is part of the Liza Data Collection Framework
+ *
+ *  Liza is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var Class = require( 'easejs' ).Class;
+
+
+/**
+ * Store pattern matching failure
+ *
+ * A key request did not match any patterns known to the Store.
+ */
+module.exports = Class( 'StorePatternError' )
+    .extend( ReferenceError, {} );
