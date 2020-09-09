1d730fff15217a7b7ce056549139a53418070ff7
Dalfonso, Anthony <anthony.dalfonso@ryansg.com>
2020-09-09 14:17:56 -0400

    Revert "Add j option for quote save"
    This reverts merge request !256

--- src/server/db/MongoServerDao.ts
+++ src/server/db/MongoServerDao.ts
@@ -326,7 +326,7 @@ export class MongoServerDao extends EventEmitter implements ServerDao {
       document,
 
       // create record if it does not yet exist
-      {upsert: true, j: true},
+      {upsert: true},
 
       // on complete
       function (err, _docs) {
